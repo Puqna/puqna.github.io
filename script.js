408a9a5380b2cf4eba5711ff2ac536f3a939ed6e
Puqna <165813068+Puqna@users.noreply.github.com>
2026-02-14 08:04:13 +0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Toggle dropdown open/close
-const dropdown = document.querySelector(".dropdown");
-const button = dropdown.querySelector(".dropbtn");
-
-button.addEventListener("click", () => {
-  dropdown.classList.toggle("show");
-});
